aca9007ce7c703114b93dcaefa662f2931964763
LeoMe34 <100803225+LeoMe34@users.noreply.github.com>
2024-01-03 17:56:40 -0600

    Add files via upload

---
+++ lib/shared/data/local_video_posts.dart
@@ -0,0 +1,54 @@
+List<Map<String, dynamic>> videoPosts = [
+  {
+    'name': 'Subiendo escaleras automáticas',
+    'videoUrl': 'assets/videos/1.mp4',
+    'likes': 23230,
+    'views': 1523,
+  },
+  {
+    'name': 'Trabajando en la laptop:)',
+    'videoUrl': 'assets/videos/2.mp4',
+    'likes': 24230,
+    'views': 1343,
+  },
+  {
+    'name': 'Viajando en carretera!!!',
+    'videoUrl': 'assets/videos/3.mp4',
+    'likes': 21564320,
+    'views': 123563,
+  },
+  {
+    'name': 'Tiempo de pizza con la banda',
+    'videoUrl': 'assets/videos/4.mp4',
+    'likes': 320,
+    'views': 2300,
+  },
+  {
+    'name': 'Perro',
+    'videoUrl': 'assets/videos/5.mp4',
+    'likes': 3730,
+    'views': 44030,
+  },
+  {
+    'name': 'Paz interior',
+    'videoUrl': 'assets/videos/6.mp4',
+    'likes': 10000,
+    'views': 3304858,
+  },
+  {
+    'name': 'Playa',
+    'videoUrl': 'assets/videos/7.mp4',
+    'likes': 1320,
+    'views': 33032,
+  },
+  {
+    'name': 'Trafico',
+    'videoUrl': 'assets/videos/8.mp4',
+    'likes': 3422,
+    'views': 33321,
+  },
+
+
+];
+
+
